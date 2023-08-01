30950a3c7e5d1f6aace248da47fc453fad7c3bf7
kratisaxena29 <106070526+kratisaxena29@users.noreply.github.com>
2023-08-01 16:11:53 +0530

    Adding frontend part with userValidation

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://0.0.0.0/Registration", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(() => {
+    console.log(`connection successful`);
+}).catch((e) => {
+    console.log(`no connection`);
+})
